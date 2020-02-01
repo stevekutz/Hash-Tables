6e14826c08f83d64af207e268db7cd6fbb3824f4
stevekutz <stkutz@gmail.com>
2020-01-31 21:53:43 -0600

    completed MVP, added chaining debug info

--- src/hashtable.py
+++ src/hashtable.py
@@ -42,6 +42,7 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
+        print("\n ******* ")
         return self._hash(key) % self.capacity
 
 
@@ -58,26 +59,25 @@ class HashTable:
         
         # generate hash for index
 
-        print("\n++++++++++++++++++")
+        # print("\n++++++++++++++++++")
         index = self._hash_mod(key)
-        print(" index is:", index)
+        print("index is:", index)
         print("value: ", value)
         print("hash-key: ", self._hash(key))    
         print("hash-mod: ", self._hash(key) % self.capacity)
 
 
-        # verify if new LL  self.storage[0] = None, else or add to end 
+        # verify if new LL  self.storage[0] = None, else or add to head!!
         if self.storage[index] == None:
             self.storage[index] = LinkedPair(key, value)
         else:
             if(self.storage[index].next == None):
-                print(" Collision at index: ", index)
-            orig = self.storage[index]
-            self.storage[index] = LinkedPair(key,value)
-            self.storage[index].next = orig 
+                print("!!!!!!  Collision at index: ", index)
+            old_head = self.storage[index]
 
-
-
+            self.storage[index] = LinkedPair(key,value) # set up new node
+            self.storage[index].next = old_head #
+            print("current head value: %s: " % ( self.storage[index].value ) )
 
     def remove(self, key):
         '''
@@ -87,8 +87,26 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        ll_node = 0
 
+        if self.storage[index] is None:
+            return "Key does not exist in Hash Table, nothing to remove"
+        else:
+            if self.storage[index].key == key:
+                print("this key was removed at head location",  key)
+                self.storage[index].value = None
+            else:    
+                current = self.storage[index].next # get next node location
+                while current is not None:
+                    ll_node += 1
+                    if current.key == key:
+                        current.value = None
+                        return ("removed node with key: %s at ll node: %i" % (current.key, ll_node))
+                    current = current.next     
+
+                if current is None:
+                    return "Key does not exist in Hash Table, nothing to remove"    
 
     def retrieve(self, key):
         '''
@@ -98,7 +116,32 @@ class HashTable:
 
         Fill this in.
         '''
-        
+        index = self._hash_mod(key)
+        ll_node = 0
+
+        if self.storage[index] is None:
+            return None
+
+        # loop through hash table & chain for key
+        else:
+            if self.storage[index].key == key:
+                print("key %s found at hash table index %i : " % (key, index))
+                return self.storage[index].value
+
+            else:   # iterate through ll chain
+                current = self.storage[index].next
+                while current is not None:    
+                    ll_node += 1
+                    if current.key == key:
+                        print("hash table index: ", index)
+                        print("ll node location is: ", ll_node)
+                        #  print(">>>>>  this is it: " , current.value)
+                        return current.value
+                    else:
+                        current = current.next    
+                if current is None:
+                    return None
+        # 
 
 
     def resize(self):
@@ -128,24 +171,37 @@ if __name__ == "__main__":
     ht.insert("line_1", "Tiny hash table")
     ht.insert("line_2", "Filled beyond capacity")
     ht.insert("line_3", "Linked list saves the day!")
+    ht.insert("line_4", ">> gonna remove this one !!")
 
-    # print("")
+    print("")
 
     # # Test storing beyond capacity
-    # print(ht.retrieve("line_1"))
-    # print(ht.retrieve("line_2"))
-    # print(ht.retrieve("line_3"))
+    print(ht.retrieve("line_1"))
+    print(ht.retrieve("line_2"))
+    print(ht.retrieve("line_3"))
+    print(ht.retrieve("line_4"))
 
+    print(ht.remove("Does not exist"))
+    print(ht.remove("line_4"))
+    # print(ht.remove("line_3"))
+    # print(ht.remove("line_1"))
+
+    print(ht.retrieve("line_1"))
+    print(ht.retrieve("line_2"))
+    print(ht.retrieve("line_3"))
+    print(ht.retrieve("line_4"))
+
+    print(ht.retrieve("Does not exist"))
     # Test resizing
-    # old_capacity = len(ht.storage)
-    # ht.resize()
-    # new_capacity = len(ht.storage)
+    old_capacity = len(ht.storage)
+    ht.resize()
+    new_capacity = len(ht.storage)
 
-    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
     # # Test if data intact after resizing
-    # print(ht.retrieve("line_1"))
-    # print(ht.retrieve("line_2"))
-    # print(ht.retrieve("line_3"))
+    print(ht.retrieve("line_1"))
+    print(ht.retrieve("line_2"))
+    print(ht.retrieve("line_3"))
 
-    # print("")
+    print("")
